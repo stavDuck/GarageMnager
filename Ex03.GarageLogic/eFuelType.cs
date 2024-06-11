c245cf4850028eec8b74e14cabf0c7333318f7d9
shakedhl17 <104508136+shakedhl17@users.noreply.github.com>
2024-06-11 17:03:27 +0300

    added style

--- Ex03.GarageLogic/eFuelType.cs
+++ Ex03.GarageLogic/eFuelType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ex03.GarageLogic
+﻿namespace Ex03.GarageLogic
 {
     public enum eFuelType
     {
