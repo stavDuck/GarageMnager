f3e9b89b5bdf6b3ac0de33d5583de78f030b885a
shakedhl17 <104508136+shakedhl17@users.noreply.github.com>
2024-06-12 10:27:30 +0300

    added Energy classes

--- Ex03.GarageLogic/ElectricContainer.cs
+++ Ex03.GarageLogic/ElectricContainer.cs
@@ -4,5 +4,21 @@ namespace Ex03.GarageLogic
 {
     public class ElectricContainer : EnergyContainer
     {
+        public ElectricContainer(float i_MaxEnergyAmount) : base(i_MaxEnergyAmount)
+        {
+
+        }
+
+        public override void AddEnergy(float i_energyAmount, eFuelType? i_FuelTypeToAdd = null)
+        {
+            if (i_energyAmount< 0 || m_CurrentEnergyAmount + i_energyAmount> r_MaxEnergyAmount)
+            {
+                throw new ValueOutOfRangeException(0, r_MaxEnergyAmount - m_CurrentEnergyAmount);
+            }
+            else
+            {
+                m_CurrentEnergyAmount += i_energyAmount;
+            }
+        }
     }
 }
